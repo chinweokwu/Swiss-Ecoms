6e3d7e1a5b9335f9c217ac970babbb6254a3adfd
chinweokwu <paulcmorah@gmail.com>
2023-02-06 23:47:55 +0100

    add featured, carousel & category component

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const  Navbar = () => {
 
   return (
     <>
-      <nav className=" flex flex-wrap items-center justify-between px-2 py-3 mb-3">
+      <nav className="h-18 bg-gradient-to-r from-gray-100 via-[#bce1ff] to-gray-100 flex flex-wrap items-center justify-between px-2 py-3 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div>
             <Link
@@ -16,17 +16,13 @@ const  Navbar = () => {
               Swiss Socials
             </Link>
           </div>
-
           <button
             className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
             type="button" onClick={() => setNavbarOpen(!navbarOpen)}>
             <i className="fas fa-bars"></i>
           </button>
-
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            
             <div className={ "lg:flex flex-grow items-center" + (navbarOpen ? " flex" : " hidden") } id="example-navbar-danger">
-
               <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
                 <li className="nav-item">
                   <Link        
@@ -55,11 +51,9 @@ const  Navbar = () => {
             </ul>
             </div>
           </div>
-
-    <div className="lg:block hidden grow">
-      <h1 className="flex justify-center ">Swiss</h1>
-    </div>
-
+          <div className="lg:block hidden grow">
+            <h1 className="flex justify-center ">Swiss</h1>
+          </div>
           <div
             className={
               "lg:flex items-center" +
@@ -103,7 +97,6 @@ const  Navbar = () => {
             </ul>
           </div>
         </div>
-
       </nav>
     </>
   );
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import {CarouselItem, Carousel} from '../../Components/Carousel/Carousel';
+import Featured from '../../Components/Featured/Featured';
+import Category from '../../Components/Category/Category';
+const img = [
+  {
+    id: 1,
+    link: 'https://images.pexels.com/photos/3965557/pexels-photo-3965557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 2,
+    link: 'https://images.pexels.com/photos/374677/pexels-photo-374677.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    id: 3,
+    link: 'https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  },
+  {
+    id: 4,
+    link: 'https://images.pexels.com/photos/942317/pexels-photo-942317.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  }
 
+]
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Carousel>
+        {img.map((item) => (
+          <CarouselItem key={item.id}>
+            <img src={item.link} alt="img" />
+          </CarouselItem>
+        ))}
+      </Carousel>
+      <Featured />
+      <Category />
+    </div>
   )
 }
 
